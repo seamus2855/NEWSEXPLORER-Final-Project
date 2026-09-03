01f0c32185088569d94fae1dec577b4b480a5979
Seamus Ryan <seamusryan787@gmail.com>
2026-09-02 20:49:11 -0400

    sprint 16

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './src/components/App/App.js'; // Updated path structure standard for TripleTen projects
+import App from './components/App/App.jsx'; // Removed /src and updated to .jsx
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <BrowserRouter>
