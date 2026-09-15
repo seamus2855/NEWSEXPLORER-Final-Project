a4e5beac4a2e3e0c9a2f0eab5a744da77b3571b4
Seamus Ryan <seamusryan787@gmail.com>
2026-09-15 17:39:38 -0400

    final project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/NEWSEXPLORER-Final-Project/", // 👈 Informs Vite where static paths are hosted on GitHub Pages
+});
